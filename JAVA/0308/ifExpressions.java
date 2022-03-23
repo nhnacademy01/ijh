b4236e4486695017dd0d0e4418266596a64c1b98
IJH <whdgus0515@gmail.com>
2022-03-23 10:25:31 +0900

    0323JsStart

--- JAVA/0308/ifExpressions.java
+++ JAVA/0308/ifExpressions.java
@@ -1,29 +1,50 @@
-public class ifExpressions {
-    static boolean isSpring(int month)
-    {
-        return month >=3 && month <=5;
+public class IfExpressions {
+    public static void main(String[] args) {
+        IfExpressions i = new IfExpressions();
+        i.switchPrint(65);
     }
 
-    public static void main(String[] args) {
-        int month =3;
+    void elseIfToTriOperator(){
+        int score =50;
+        String grade;
 
-        if(isSpring(month))
-        {
-            System.out.println("flowers bloom");
+        grade = score >= 90 ? "A" :
+                score >= 80 ? "B" :
+                        "F";
+        System.out.println("Grade : " + grade);
+    }
+
+    void print(int score){
+        if(score >= 90){
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
         }
     }
-}
 
-class test
-{
-    static boolean isSpring(int month)
-    {
-        return month >=3 && month <=5;
-    }
-    int month =3;
-    boolean isSpring = isSpring(month);
-
-    public static void main(String[] args) {
-
+    void switchPrint(int score){
+        switch (score / 10) {
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+        }
     }
 }
